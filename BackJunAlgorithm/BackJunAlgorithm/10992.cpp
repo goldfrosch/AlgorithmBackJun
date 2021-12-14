89d99d1e16c0978a39d0b3943356df8668a33b12
goldfrosch <goldfrosch@daum.net>
2021-12-14 20:41:41 +0900

    GoldFrosch / 입출력 10992 해결!

---
+++ BackJunAlgorithm/BackJunAlgorithm/10992.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include<cstdlib>
+
+using namespace std;
+
+//¹®Á¦ https://www.acmicpc.net/problem/10992
+int main()
+{
+    int num;
+    std::cin >> num;
+    
+    for (int i = 1; i <= num; i++) {
+        for (int j = 1; j <= num * 2 - 1; j++) {
+            if (i == num) {
+                std::cout << "*";
+            }
+            else {
+                if (abs(num - j) == i - 1) {
+                    std::cout << "*";
+                }
+                else {
+                    std::cout << " ";
+                }
+            }
+        }
+        printf("\n");
+    }
+    return 0;
+}
